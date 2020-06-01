637039b0a1e2c10357fc122df3c7d20eafe47572
kyori19 <kyori@accelf.net>
2020-06-02 00:33:05 +0900

    Initial commit

---
+++ app/src/main/java/net/accelf/itc_lms_unofficial/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.accelf.itc_lms_unofficial
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
