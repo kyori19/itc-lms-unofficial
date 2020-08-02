806ef1602be77c8adb38c67cafc0d0dd45aa2498
kyori19 <kyori@accelf.net>
2020-08-03 00:21:17 +0900

    Disable login button when inputs are empty

--- app/src/main/java/net/accelf/itc_lms_unofficial/LoginRequestFragment.kt
+++ app/src/main/java/net/accelf/itc_lms_unofficial/LoginRequestFragment.kt
@@ -1,6 +1,8 @@
 package net.accelf.itc_lms_unofficial
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_login_request.*
@@ -13,15 +15,39 @@ class LoginRequestFragment : Fragment(R.layout.fragment_login_request) {
 
         textLoginInstruction.text = getString(R.string.login_instruction)
 
-        buttonLogin.setOnClickListener {
-            val userName = editTextUserName.text?.trim()
-            val password = editTextPassword.text?.trim()
-            if (userName.isNotNullOrEmpty() && password.isNotNullOrEmpty()
-                && activity is LoginActivity
-            ) {
-                (activity as LoginActivity).onLoginClick(userName.toString(), password.toString())
+        val textWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                buttonLogin.isEnabled = editTextContents().first
             }
         }
+        listOf(editTextUserName, editTextPassword).forEach {
+            it.addTextChangedListener(textWatcher)
+        }
+
+        buttonLogin.apply {
+            isEnabled = false
+
+            setOnClickListener {
+                val (areNullOrEmpty, userName, password) = editTextContents()
+                if (areNullOrEmpty && activity is LoginActivity) {
+                    (activity as LoginActivity).onLoginClick(userName, password)
+                }
+            }
+        }
+    }
+
+    private fun editTextContents(): Triple<Boolean, String, String> {
+        val userName = editTextUserName.text?.trim()
+        val password = editTextPassword.text?.trim()
+        return Triple(
+            userName.isNotNullOrEmpty() && password.isNotNullOrEmpty(),
+            userName.toString(),
+            password.toString()
+        )
     }
 
     companion object {
