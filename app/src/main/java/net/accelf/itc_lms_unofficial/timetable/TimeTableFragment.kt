3b1caaca5d3b0df95286e055aef7949bb3195fbb
kyori19 <kyori@accelf.net>
2020-09-19 22:15:38 +0900

    Fix SwipeRefreshLayout too sensitive

--- app/src/main/java/net/accelf/itc_lms_unofficial/timetable/TimeTableFragment.kt
+++ app/src/main/java/net/accelf/itc_lms_unofficial/timetable/TimeTableFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_base.*
 import kotlinx.android.synthetic.main.fragment_time_table.*
@@ -24,12 +25,31 @@ class TimeTableFragment : Fragment(R.layout.fragment_time_table) {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel.timeTable.onSuccess(this) {
+            val swipeRefresh = (activity as MainActivity).swipeRefresh
+
             if (viewPager.adapter == null) {
                 viewPager.adapter = TimeLinesAdapter(this, it.courses)
 
                 TabLayoutMediator(tabLayout, viewPager) { tab, index ->
                     tab.text = TimeTable.DayOfWeek.fromIndex(index).toString()
                 }.attach()
+
+                viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                    override fun onPageScrollStateChanged(state: Int) {
+                        super.onPageScrollStateChanged(state)
+
+                        when (state) {
+                            ViewPager2.SCROLL_STATE_DRAGGING -> {
+                                swipeRefresh.isEnabled = false
+                            }
+                            ViewPager2.SCROLL_STATE_IDLE,
+                            ViewPager2.SCROLL_STATE_SETTLING,
+                            -> {
+                                swipeRefresh.isEnabled = true
+                            }
+                        }
+                    }
+                })
             }
 
             if (pickerYear.adapter == null) {
@@ -52,7 +72,7 @@ class TimeTableFragment : Fragment(R.layout.fragment_time_table) {
 
                 listOf(pickerYear, pickerTerm).forEach { picker ->
                     picker.setOnItemClickListener { _, _, _, _ ->
-                        (activity as MainActivity).swipeRefresh.isRefreshing = true
+                        swipeRefresh.isRefreshing = true
                         viewModel.load(
                             it.years.valueFor(pickerYear.text.toString()),
                             it.terms.valueFor(pickerTerm.text.toString()),
