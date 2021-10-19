9da49942643dd8a077d5377ebc9bfb69f034c6a9
kyori19 <kyori@accelf.net>
2021-10-20 02:23:09 +0900

    Remove unused function

--- app/src/main/java/net/accelf/itc_lms_unofficial/models/QuillData.kt
+++ app/src/main/java/net/accelf/itc_lms_unofficial/models/QuillData.kt
@@ -1,11 +1,5 @@
 package net.accelf.itc_lms_unofficial.models
 
-import android.graphics.Color
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
-import android.text.style.URLSpan
 import com.google.gson.Gson
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
@@ -13,30 +7,6 @@ import java.io.Serializable
 data class QuillData(
     @SerializedName("ops") val content: List<QuillElement>,
 ) : Serializable {
-
-    fun toSpanned(): Spanned {
-        val builder = SpannableStringBuilder()
-        content.forEach {
-            val start = builder.length
-            builder.append(it.text)
-            val end = builder.length
-            it.attributes?.let { attrs ->
-                attrs.color?.let { color ->
-                    kotlin.runCatching { Color.parseColor(color) }
-                        .onSuccess { parsedColor ->
-                            builder.setSpan(ForegroundColorSpan(parsedColor),
-                                start,
-                                end,
-                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                        }
-                }
-                attrs.link?.let { link ->
-                    builder.setSpan(URLSpan(link), start, end, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                }
-            }
-        }
-        return builder
-    }
 
     data class QuillAttributes(
         val color: String?,
