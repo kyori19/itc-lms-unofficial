761a57b9f0abcdc620dc57953d04f5486b63664e
kyori19 <kyori@accelf.net>
2022-04-13 14:57:47 +0900

    Fix notification worker fails to parse html

--- app/src/main/java/net/accelf/itc_lms_unofficial/models/Updates.kt
+++ app/src/main/java/net/accelf/itc_lms_unofficial/models/Updates.kt
@@ -5,7 +5,6 @@ import net.accelf.itc_lms_unofficial.models.Update.ActionType.Companion.toAction
 import net.accelf.itc_lms_unofficial.models.Update.ContentType.Companion.toContentType
 import net.accelf.itc_lms_unofficial.models.Update.Role.Companion.toRole
 import net.accelf.itc_lms_unofficial.network.DocumentConverterFactory
-import net.accelf.itc_lms_unofficial.util.second
 import net.accelf.itc_lms_unofficial.util.toDateTime
 import okhttp3.ResponseBody
 import java.io.Serializable
@@ -25,24 +24,25 @@ data class Updates(
                     document.select(".updateInfoTable .updateTableContents .updateInfoCell")
                         .map { row ->
                             lateinit var url: String
+                            lateinit var text: String
+                            row.select("button").first()?.let { element ->
+                                url = element.`val`()
+                                text = element.select("span").text()
+                            }
+
                             lateinit var courseName: String
                             val periods = mutableListOf<Pair<TimeTable.DayOfWeek, Int>>()
-                            lateinit var text: String
-                            row.select(".message_link button").first()?.let { element ->
-                                url = element.`val`()
-                                element.select("span").first()?.text()
-                                    ?.let { COURSE_NAME_REGEX.matchEntire(it) }
-                                    ?.let {
-                                        it.groupValues[1].split("・").forEach { periodText ->
-                                            PERIOD_REGEX.matchEntire(periodText)?.let { result ->
-                                                periods.add(result.groupValues[1].toDow() to (result.groupValues[2].toIntOrNull()
-                                                    ?: -1))
-                                            }
+                            row.select(".studentCourseInfo").first()?.text()
+                                ?.let { COURSE_NAME_REGEX.matchEntire(it) }
+                                ?.let {
+                                    it.groupValues[1].split("・").forEach { periodText ->
+                                        PERIOD_REGEX.matchEntire(periodText)?.let { result ->
+                                            periods.add(result.groupValues[1].toDow() to (result.groupValues[2].toIntOrNull()
+                                                ?: -1))
                                         }
-                                        courseName = it.groupValues[2]
                                     }
-                                text = element.select("span").second().text()
-                            }
+                                    courseName = it.groupValues[2]
+                                }
 
                             Update(
                                 row.select("input#updateInfoId").first()?.`val`() ?: "",
@@ -68,6 +68,6 @@ data class Updates(
         private val PERIOD_REGEX =
             Regex("""(Mon|Tue|Wed|Thu|Fri|Sat|Other|[月火水木金土他])([\d０-９他]|Other)""")
         private val COURSE_NAME_REGEX =
-            Regex("""\[((?:Mon|Tue|Wed|Thu|Fri|Sat|Other|[月火水木金土他])(?:[\d０-９他]|Other)(?:・(?:Mon|Tue|Wed|Thu|Fri|Sat|Other|[月火水木金土他])(?:[\d０-９他]|Other))*)\s(.+)]""")
+            Regex("""((?:Mon|Tue|Wed|Thu|Fri|Sat|Other|[月火水木金土他])(?:[\d０-９他]|Other)(?:・(?:Mon|Tue|Wed|Thu|Fri|Sat|Other|[月火水木金土他])(?:[\d０-９他]|Other))*)\s(.+)""")
     }
 }
